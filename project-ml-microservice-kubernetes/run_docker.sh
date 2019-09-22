c68c2e86bec77c5d11c5acc9a2701df32b66a712
Florian Seidel <seidel.florian@gmail.com>
2019-09-22 13:14:12 +0200

    Task 1

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
+health_check () {
+  until $(curl --output /dev/null --silent --head --fail $1); do
+    printf '.'
+    sleep 1
+  done
+}
+
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+#Tag defined in .env file
+docker-compose build
 
-# Step 2: 
-# List docker images
+# Step 3:
+#Switching steps 2 and 3, because docker-compose images
+#only produces output for running stacks
+echo "Application starts in background, obtain logs by using docker-compose log"
+docker-compose up -d
+health_check "http://localhost:8000"
+echo "Application running"
 
-# Step 3: 
-# Run flask app
+# Step 2:
+
+docker-compose images
